f0573db01236889b76e998e9ba77f7c0292f27e4
Damenjo Sitepu <damenstory@gmail.com>
2023-08-13 21:52:55 +0700

    Feat: addChartOfAccount() method to create new chart of account

--- src/app/chart-of-account/service.ts
+++ src/app/chart-of-account/service.ts
@@ -1,7 +1,11 @@
 import fs from "fs";
 import path from "path";
 import { ChartOfAccount } from "./response.interface";
+import { CreateChartOfAccountsRequest } from "./request.interface";
 
+/**
+ * Read Chart of account file
+ */
 export async function readChartOfAccountFile(): Promise<ChartOfAccount[]> {
     try {
         const readFileBuffer = await fs.promises.readFile(path.join(__dirname, "chart-of-account.json"), "utf8");
@@ -10,3 +14,17 @@ export async function readChartOfAccountFile(): Promise<ChartOfAccount[]> {
         return [];
     }
 };
+
+/**
+ * Add chart of account
+ * @param req 
+ * @param data 
+ */
+export function addChartOfAccount(req: CreateChartOfAccountsRequest, data: ChartOfAccount[]): ChartOfAccount[] {
+    data.push({
+        id: data.length + 1,
+        name: req.name,
+        isActive: true
+    });
+    return data;
+}
