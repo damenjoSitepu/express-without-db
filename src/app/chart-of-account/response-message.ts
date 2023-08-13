0be9a4e006444e405286a97456cdf8b424737f9c
Damenjo Sitepu <damenstory@gmail.com>
2023-08-13 21:53:14 +0700

    Feat: add success message when creating chart of account

--- src/app/chart-of-account/response-message.ts
+++ src/app/chart-of-account/response-message.ts
@@ -1,5 +1,6 @@
 export const CHART_OF_ACCOUNT_RESPONSE_MESSAGE = {
     GET_SUCCESS: "Successfully get your chart of accounts!",
+    CREATE_SUCCESS: "Successfully creating your brand new chart of accounts!",
     SOMETHING_WENT_WRONG: "Something went wrong when fetch chart of accounts. Please try again!",
     EMPTY: "Chart of accounts not found!"
 }
