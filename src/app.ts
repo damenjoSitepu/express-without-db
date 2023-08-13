671a7e3a96f48eaf6aba3b39f526de010652b285
Damenjo Sitepu <damenstory@gmail.com>
2023-08-13 10:52:39 +0700

    Feat: append apiRoute main route inside app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import { EXPRESS_CONFIG } from "./configs/express.config";
+import { apiRoute } from "./routes/api.route";
 
 const app = express();
+
+apiRoute(app);
 
 /** Init Routes */
 app.listen(EXPRESS_CONFIG.PORT, () => {
