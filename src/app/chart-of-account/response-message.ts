362a26e212ff5f102e760bff33fb8a6d1d2bf725
Damenjo Sitepu <damenstory@gmail.com>
2023-08-13 13:58:09 +0700

    Feat: add new message (something went wrong) and (empty) message

--- src/app/chart-of-account/response-message.ts
+++ src/app/chart-of-account/response-message.ts
@@ -1,3 +1,5 @@
 export const CHART_OF_ACCOUNT_RESPONSE_MESSAGE = {
-    GET_SUCCESS: "Successfully get your chart of accounts!"
+    GET_SUCCESS: "Successfully get your chart of accounts!",
+    SOMETHING_WENT_WRONG: "Something went wrong when fetch chart of accounts. Please try again!",
+    EMPTY: "Chart of accounts not found!"
 }
