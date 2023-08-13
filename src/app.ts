0d78c2189aeb8ed183b5e75c468672ea0c4d2501
Damenjo Sitepu <damenstory@gmail.com>
2023-08-13 10:39:22 +0700

    Feat: create app.listen() to set up the app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import { EXPRESS_CONFIG } from "./configs/express.config";
 
 const app = express();
 
 /** Init Routes */
-app.listen()
+app.listen(EXPRESS_CONFIG.PORT, () => {
+    console.log("App are ready to be released");
+});
