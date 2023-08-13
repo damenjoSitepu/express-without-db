ec68119c35567d99ca89f3b9a7739f5766dbd35a
Damenjo Sitepu <damenstory@gmail.com>
2023-08-13 22:26:00 +0700

    Feat: add successfully when deleting chart of account

--- src/app/chart-of-account/response-message.ts
+++ src/app/chart-of-account/response-message.ts
@@ -1,6 +1,7 @@
 export const CHART_OF_ACCOUNT_RESPONSE_MESSAGE = {
     GET_SUCCESS: "Successfully get your chart of accounts!",
-    CREATE_SUCCESS: "Successfully creating your brand new chart of accounts!",
+    CREATE_SUCCESS: "Successfully creating your brand new chart of account!",
+    DELETE_SUCCESS: "Successfully deleting your chart of account!",
     SOMETHING_WENT_WRONG: "Something went wrong when fetch chart of accounts. Please try again!",
     EMPTY: "Chart of accounts not found!"
 }
