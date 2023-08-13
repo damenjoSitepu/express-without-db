ccbcf82a096524954cdac0f3d1d53f3247ebdf34
Damenjo Sitepu <damenstory@gmail.com>
2023-08-13 21:52:09 +0700

    Feat: allow urlencoded for data object, allow cors()

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
 import express from "express";
 import { EXPRESS_CONFIG } from "./configs/express.config";
 import { apiRoute } from "./routes/api.route";
+import cors from "cors";
 
 const app = express();
+// Allow Request From Any IP Address
+app.use(cors());
+app.use(express.urlencoded({
+    extended: false,
+    limit: 10000,
+    parameterLimit: 2
+}));
+app.use(express.json());
 
 apiRoute(app);
 
