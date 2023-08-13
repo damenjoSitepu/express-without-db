dfff4110118707542166838252d2f1206a6e1d0f
Damenjo Sitepu <damenstory@gmail.com>
2023-08-13 10:47:25 +0700

    Feat: setup main root

---
+++ src/routes/api.route.ts
@@ -0,0 +1,18 @@
+import express, {
+    Express, Request, Response, NextFunction
+} from "express";
+import { ROUTE_URL } from "../urls/route.url";
+import { MAIN_MESSAGE } from "../messages/main.message";
+
+/**
+ * Main Routes Here
+ */
+export const mainRouteApp = (app: Express) => {
+    app.get(ROUTE_URL.ROOT, (req: Request, res: Response, next: NextFunction) => {
+        return res.json({
+            error: false,
+            code: 200,
+            message: MAIN_MESSAGE.GET_SUCCESS
+        })
+    });
+};
