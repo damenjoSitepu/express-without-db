aa1c072b74715cb0975359fbf79c7cb7244fe27f
Damenjo Sitepu <damenstory@gmail.com>
2023-08-13 14:17:01 +0700

    Refactor: getChartOfAccountsHandler with strict type

--- src/app/chart-of-account/controller.ts
+++ src/app/chart-of-account/controller.ts
@@ -2,6 +2,7 @@ import { Response, Request, NextFunction } from "express";
 import { CHART_OF_ACCOUNT_RESPONSE_MESSAGE } from "./response-message";
 import fs from "fs";
 import path from "path";
+import { ChartOfAccount, ChartOfAccountsResponse } from "./response.interface";
 
 /**
  * Get Chart Of Accounts
@@ -9,10 +10,11 @@ import path from "path";
  * @param res 
  * @param next 
  */
-export const getChartOfAccountsHandler = async (req: Request, res: Response, next: NextFunction) => {
+export const getChartOfAccountsHandler = async (req: Request, res: Response<ChartOfAccountsResponse>, next: NextFunction) => {
     try { 
-        let chartOfAccounts = await fs.promises.readFile(path.join(__dirname, "chart-of-account.json"), "utf8");
-        chartOfAccounts = JSON.parse(chartOfAccounts);
+        const chartOfAccountsBuffer: string = await fs.promises.readFile(path.join(__dirname, "chart-of-account.json"), "utf8");
+        const chartOfAccounts: ChartOfAccount[] = JSON.parse(chartOfAccountsBuffer);
+        
         if (chartOfAccounts?.length === 0) {
             return res.json({
                 error: true,
@@ -25,13 +27,15 @@ export const getChartOfAccountsHandler = async (req: Request, res: Response, nex
             error: false,
             code: 200,
             message: CHART_OF_ACCOUNT_RESPONSE_MESSAGE.GET_SUCCESS,
-            data: { chartOfAccounts }
+            data: { 
+                chartOfAccounts: chartOfAccounts
+            }
         });
     } catch (e) {
         return res.json({
             error: true,
             code: 500,
-            message: ((e instanceof Error) && e.message) ?? CHART_OF_ACCOUNT_RESPONSE_MESSAGE.SOMETHING_WENT_WRONG,
+            message: e instanceof Error ? e.message : CHART_OF_ACCOUNT_RESPONSE_MESSAGE.SOMETHING_WENT_WRONG
         });
     }
 }
