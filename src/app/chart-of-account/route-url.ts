815e05869b95cd4eb4a1d498d1f1e5a83a73c038
Damenjo Sitepu <damenstory@gmail.com>
2023-08-14 06:18:56 +0700

    Feat: add update route for chart of account

--- src/app/chart-of-account/route-url.ts
+++ src/app/chart-of-account/route-url.ts
@@ -1,5 +1,6 @@
 export const CHART_OF_ACCOUNT_ROUTE_URL = {
     GET: "/",
     CREATE: "/",
-    DELETE: "/"
+    DELETE: "/",
+    UPDATE: "/"
 };
--- src/routes/Apis/chart-of-account.route.ts
+++ src/routes/Apis/chart-of-account.route.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { CHART_OF_ACCOUNT_ROUTE_URL } from "../../app/chart-of-account/route-url";
-import { createChartOfAccountHandler, deleteChartOfAccountHandler, getChartOfAccountsHandler } from "../../app/chart-of-account/controller";
+import { createChartOfAccountHandler, deleteChartOfAccountHandler, getChartOfAccountsHandler, updateChartOfAccountHandler } from "../../app/chart-of-account/controller";
 
 const router = express.Router();
 
 router.get(CHART_OF_ACCOUNT_ROUTE_URL.GET, getChartOfAccountsHandler);
 router.post(CHART_OF_ACCOUNT_ROUTE_URL.CREATE, createChartOfAccountHandler);
 router.delete(CHART_OF_ACCOUNT_ROUTE_URL.DELETE, deleteChartOfAccountHandler);
+router.put(CHART_OF_ACCOUNT_ROUTE_URL.UPDATE, updateChartOfAccountHandler);
 
 export default router;
